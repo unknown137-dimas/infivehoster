f6cf8195c77f1e768fd91e70542fa93c5f845068
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2022-11-30 17:52:35 +0700

    Update api-url-updater.py

--- infrastructure/api-url-updater/api-url-updater.py
+++ infrastructure/api-url-updater/api-url-updater.py
@@ -4,13 +4,14 @@ import re
 ngrok_url = 'http://localhost:4040/api/tunnels'
 res = requests.get(ngrok_url)
 api_public_url = res.json()['tunnels'][0]['public_url']
-print(api_public_url)
 
-with open('register.js', 'r') as api_file:
+with open('infivehoster/frontend/assets/js/register.js', 'r') as api_file:
     lines = api_file.readlines()
     for line in lines:
-        if 'https://' in line:
+        api_url = re.findall('https://[a-z0-9]{4}-[a-z0-9]{3}-[a-z0-9]{2}-[a-z0-9]{2}-[a-z0-9]{2}\.[a-z0-9]{2}\.ngrok\.io', line)
+        if api_url != api_public_url:
             new = re.sub('https://[a-z0-9]{4}-[a-z0-9]{3}-[a-z0-9]{2}-[a-z0-9]{2}-[a-z0-9]{2}\.[a-z0-9]{2}\.ngrok\.io', api_public_url, line)
             lines[lines.index(line)] = new
-    with open('register.js', 'w') as output_api_file:
-        output_api_file.writelines(lines)
+            with open('register.js', 'w') as output_api_file:
+                output_api_file.writelines(lines)
+        break
